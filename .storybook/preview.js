85f3f51329b8b5d9836807ff525f3611551f1ee0
Matej Hrescak <matej@hrescak.com>
2021-02-09 11:22:56 -0500

    added global style to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import React from 'react'
+import BaseStyle from '../src/components/BaseStyle'
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  Story => (
+    <>
+      <BaseStyle />
+      <Story />
+    </>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
