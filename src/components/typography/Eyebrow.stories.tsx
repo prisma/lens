1aa92f7c00fb9ed357e009d618756fbd215eb063
Matej Hrescak <matej@hrescak.com>
2021-02-10 15:28:45 -0500

    Intro page

--- src/components/typography/Eyebrow.stories.tsx
+++ src/components/typography/Eyebrow.stories.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Eyebrow from "./Eyebrow";
-import { H2 } from "./heading";
 
 export default {
   title: "Typography/Eyebrow",
@@ -10,6 +9,5 @@ export default {
 export const Default = () => (
   <>
     <Eyebrow>This is an Eyebrow</Eyebrow>
-    <H2 style={{ margin: 0 }}>This is a title</H2>
   </>
 );
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default } from './theme'
 export { default as BaseStyle } from './components/BaseStyle'
+export { default as Eyebrow } from './components/typography/Eyebrow'
 export { default as Header } from './components/websites/header'
 export { default as Footer } from './components/websites/footer'
