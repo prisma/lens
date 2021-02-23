0bccee8e9036cb5cc93199f498c49a45615ac8cb
Siddhant <me@madebysid.com>
2021-02-23 21:04:07 +0100

    emit typescript declarations

--- rollup.config.js
+++ rollup.config.js
@@ -23,6 +23,8 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({
+      clean: true,
+    }),
   ],
 };
