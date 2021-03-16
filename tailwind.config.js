7d3e1bcda6ead784ba981caf4a5f485a40645e52
Siddhant <me@madebysid.com>
2021-03-16 12:05:32 +0100

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: "media",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Barlow", "sans-serif"],
+        mono: ["monospace"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
