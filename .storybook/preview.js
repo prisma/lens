1e432b3e132bcd288a97e18194d026f7db3a401e
Siddhant <me@madebysid.com>
2021-03-16 19:45:20 +0100

    setup dark mode previews

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { themes } from "@storybook/theming";
 import { createGlobalStyle } from "styled-components";
 
-import { theme } from "../src/theme";
+import { theme as lensTheme } from "../src/theme";
 import { LensProvider } from "../src/provider/LensProvider";
 
 // TODO:: remove when we have proper normalize in BaseStyle
 const TemporaryGlobalStyle = createGlobalStyle`
  html{
-  font-family: ${theme.fonts.text}
+  font-family: ${lensTheme.fonts.text}
  }
 `;
 
@@ -23,4 +24,9 @@ export const decorators = [
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   layout: "centered",
+  darkMode: {
+    darkClass: "dark",
+    dark: { ...themes.dark, appContentBg: "#12161e" },
+    stylePreview: true,
+  },
 };
