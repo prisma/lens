04249360b2d9bfb3b4b964cdc56fb2b344dbccbc
Siddhant <me@madebysid.com>
2021-03-19 10:24:08 +0100

    Show package version in stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,10 +2,18 @@ import React from "react";
 import { themes } from "@storybook/theming";
 import { LensProvider } from "../src/provider/LensProvider";
 
+const pkg = require("../package.json");
+const version = pkg.version;
+
 export const decorators = [
   Story => (
     <LensProvider>
-      <Story />
+      <div className="fixed top-0 left-0 p-2 text-xs text-gray-500">
+        {version}
+      </div>
+      <div className="flex flex-col" style={{ width: 580 }}>
+        <Story />
+      </div>
     </LensProvider>
   ),
 ];
