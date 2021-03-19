de663c7e4d7b0d83e1d2ce540db4346690f04b11
Siddhant <me@madebysid.com>
2021-03-19 10:24:59 +0100

    Rename PageTitle to TItleGroup

--- src/components/title-group/TitleGroup.stories.tsx
+++ src/components/title-group/TitleGroup.stories.tsx
@@ -1,8 +1,8 @@
 import { storiesOf } from "@storybook/react";
-import { PageTitle } from "./PageTitle";
+import { TitleGroup } from "./TitleGroup";
 
-storiesOf("Lens/PageTitle", module).add("Default", () => (
-  <PageTitle
+storiesOf("Lens/TitleGroup", module).add("Default", () => (
+  <TitleGroup
     icon="folder-plus"
     title="New Project"
     subtitle="Create a new project or connect to an existing one on Github"
