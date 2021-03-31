a44a2d118e583cf8f793449c1b7f4f31bf97f42a
Siddhant <me@madebysid.com>
2021-03-31 18:15:34 +0200

    Remove Lens version from stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,15 +2,9 @@ import React from "react"
 import { themes } from "@storybook/theming"
 import { LensProvider } from "../src/provider/LensProvider"
 
-const pkg = require("../package.json")
-const version = pkg.version
-
 export const decorators = [
   (Story) => (
     <LensProvider>
-      <div className="fixed top-0 left-0 p-2 text-xs text-gray-500">
-        {version}
-      </div>
       <div className="flex justify-center" style={{ minWidth: 580 }}>
         <Story />
       </div>
