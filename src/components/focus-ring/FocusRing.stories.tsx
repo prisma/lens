861ef98372bfa2adcab71961b083e2d9ac043b7a
Siddhant <siddhant@hey.com>
2021-04-16 15:56:36 +0200

    FocusRing

--- src/components/focus-ring/FocusRing.stories.tsx
+++ src/components/focus-ring/FocusRing.stories.tsx
@@ -1,10 +1,14 @@
-import { storiesOf } from "@storybook/react"
 import { FocusRing } from "./FocusRing"
 
-storiesOf("Lens/FocusRing", module).add("Default", () => (
-  <FocusRing within>
+export const Default = (props) => (
+  <FocusRing within {...props}>
     <div tabIndex={1} className="rounded-md text-gray-800 dark:text-gray-100">
       Press Tab to focus me
     </div>
   </FocusRing>
-))
+)
+Default.storyName = "[Controlled]"
+export default {
+  title: "Lens/FocusRing",
+  component: FocusRing,
+}
