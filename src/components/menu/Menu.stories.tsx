3cadbb6c6233df16c2171e3ce45139fd7cbf453b
Siddhant <siddhant@hey.com>
2021-04-16 17:05:36 +0200

    Menu

--- src/components/menu/Menu.stories.tsx
+++ src/components/menu/Menu.stories.tsx
@@ -2,6 +2,7 @@ import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
 import { Menu, MenuOption } from "./Menu"
 import { Button } from "../button/Button"
+import { Icon } from "../icon/Icon"
 
 type Action =
   | "edit"
@@ -56,69 +57,101 @@ const dynamicDataInviteSectionData: MenuOption<Action>[] = [
   },
 ]
 
-storiesOf("Lens/Menu", module)
-  .add("Default (with static data)", () => (
-    <div className="flex justify-center">
-      <Menu.Container>
-        <Button variant="quiet">Menu Trigger</Button>
+export const Default = (props) => (
+  <Menu.Container>
+    <Button variant="quiet">
+      <Icon name="sliders" />
+    </Button>
 
-        <Menu.Body
-          title="Project Actions"
-          onSelectionChange={action("onSelectionChange")}
-        >
-          <Menu.Option key="edit">Edit</Menu.Option>
-          <Menu.Option key="invite">Invite</Menu.Option>
-          <Menu.Option key="delete">Delete</Menu.Option>
-        </Menu.Body>
-      </Menu.Container>
-    </div>
-  ))
-  .add("Default (with dynamic data)", () => (
-    <Menu.Container>
-      <Button variant="quiet">Menu Trigger</Button>
+    <Menu.Body
+      title="Project Actions"
+      onSelectionChange={action("onSelectionChange")}
+    >
+      <Menu.Option key="edit">Edit</Menu.Option>
+      <Menu.Option key="invite">Invite</Menu.Option>
+      <Menu.Option key="delete">Delete</Menu.Option>
+    </Menu.Body>
+  </Menu.Container>
+)
+Default.storyName = "[Controlled]"
+export default {
+  title: "Lens/Menu",
+  component: Menu,
+}
 
-      <Menu.Body
-        title="Project Actions"
-        options={dynamicData}
-        onSelectionChange={action("onSelectionChange")}
-      >
-        {(item) => <Menu.Option key={item.key}>{item.title}</Menu.Option>}
-      </Menu.Body>
-    </Menu.Container>
-  ))
-  .add("Default with Sections (with static data)", () => (
+export const WithStaticData = () => (
+  <div className="flex justify-center">
     <Menu.Container>
-      <Button variant="quiet">Menu Trigger</Button>
+      <Button variant="quiet">
+        <Icon name="sliders" />
+      </Button>
 
       <Menu.Body
         title="Project Actions"
         onSelectionChange={action("onSelectionChange")}
       >
-        <Menu.Section title="Personal">
-          <Menu.Option key="cloud">Cloud</Menu.Option>
-          <Menu.Option key="studio">Studio</Menu.Option>
-        </Menu.Section>
-        <Menu.Section title="Organizations">
-          <Menu.Option key="prisma">Prisma</Menu.Option>
-          <Menu.Option key="netlify">Netlify</Menu.Option>
-        </Menu.Section>
+        <Menu.Option key="edit">Edit</Menu.Option>
+        <Menu.Option key="invite">Invite</Menu.Option>
+        <Menu.Option key="delete">Delete</Menu.Option>
       </Menu.Body>
     </Menu.Container>
-  ))
-  .add("Default with Sections (with dynamic data)", () => (
-    <Menu.Container>
-      <Button variant="quiet">Menu Trigger</Button>
+  </div>
+)
 
-      <Menu.Body
-        title="Project Actions"
-        onSelectionChange={action("onSelectionChange")}
-      >
-        <Menu.Section items={dynamicDataMetaSectionData} title="Meta">
-          {(option) => <Menu.Option>{option.title}</Menu.Option>}
-        </Menu.Section>
-        <Menu.Section items={dynamicDataInviteSectionData} title="Invite">
-          {(option) => <Menu.Option>{option.title}</Menu.Option>}
-        </Menu.Section>
-      </Menu.Body>
-    </Menu.Container>
-  ))
+export const WithDynamicData = () => (
+  <Menu.Container>
+    <Button variant="quiet">
+      <Icon name="sliders" />
+    </Button>
+
+    <Menu.Body
+      title="Project Actions"
+      options={dynamicData}
+      onSelectionChange={action("onSelectionChange")}
+    >
+      {(item) => <Menu.Option key={item.key}>{item.title}</Menu.Option>}
+    </Menu.Body>
+  </Menu.Container>
+)
+
+export const WithSectionsAndStaticData = () => (
+  <Menu.Container>
+    <Button variant="quiet">
+      <Icon name="sliders" />
+    </Button>
+
+    <Menu.Body
+      title="Project Actions"
+      onSelectionChange={action("onSelectionChange")}
+    >
+      <Menu.Section title="Personal">
+        <Menu.Option key="cloud">Cloud</Menu.Option>
+        <Menu.Option key="studio">Studio</Menu.Option>
+      </Menu.Section>
+      <Menu.Section title="Organizations">
+        <Menu.Option key="prisma">Prisma</Menu.Option>
+        <Menu.Option key="netlify">Netlify</Menu.Option>
+      </Menu.Section>
+    </Menu.Body>
+  </Menu.Container>
+)
+
+export const WithSectionsAndDynamicData = () => (
+  <Menu.Container>
+    <Button variant="quiet">
+      <Icon name="sliders" />
+    </Button>
+
+    <Menu.Body
+      title="Project Actions"
+      onSelectionChange={action("onSelectionChange")}
+    >
+      <Menu.Section items={dynamicDataMetaSectionData} title="Meta">
+        {(option) => <Menu.Option>{option.title}</Menu.Option>}
+      </Menu.Section>
+      <Menu.Section items={dynamicDataInviteSectionData} title="Invite">
+        {(option) => <Menu.Option>{option.title}</Menu.Option>}
+      </Menu.Section>
+    </Menu.Body>
+  </Menu.Container>
+)
