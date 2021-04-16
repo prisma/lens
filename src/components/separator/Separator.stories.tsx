0b668493ee00580023b97efbe6ea52d1eeb37f17
Siddhant <siddhant@hey.com>
2021-04-16 17:48:39 +0200

    Separator

--- src/components/separator/Separator.stories.tsx
+++ src/components/separator/Separator.stories.tsx
@@ -3,26 +3,48 @@ import { action } from "@storybook/addon-actions"
 import { Separator } from "./Separator"
 import { Button } from "../button/Button"
 
-storiesOf("Lens/Separator", module)
-  .add("Default", () => (
-    <div className="flex flex-col justify-center">
-      <Button variant="quiet" onPress={action("onPress")}>
-        Top
-      </Button>
-      <Separator />
-      <Button variant="quiet" onPress={action("onPress")}>
-        Bottom
-      </Button>
-    </div>
-  ))
-  .add("Vertical", () => (
-    <>
-      <Button variant="quiet" onPress={action("onPress")}>
-        Left
-      </Button>
-      <Separator orientation="vertical" />
-      <Button variant="quiet" onPress={action("onPress")}>
-        Right
-      </Button>
-    </>
-  ))
+export const Default = (props) => (
+  <div className="flex h-10 w-10">
+    <div>Leading</div>
+    <Separator {...props} />
+    <div>Trailing</div>
+  </div>
+)
+Default.storyName = "[Controlled]"
+export default {
+  title: "Lens/Separator",
+  component: Separator,
+  argTypes: {
+    orientation: {
+      control: {
+        type: "inline-radio",
+        options: ["horizontal", "vertical"],
+      },
+      defaultValue: "horizontal",
+    },
+  },
+}
+
+export const Horizontal = () => (
+  <div className="flex flex-col justify-center">
+    <Button variant="quiet" onPress={action("onPress")}>
+      Top
+    </Button>
+    <Separator />
+    <Button variant="quiet" onPress={action("onPress")}>
+      Bottom
+    </Button>
+  </div>
+)
+
+export const Vertical = () => (
+  <>
+    <Button variant="quiet" onPress={action("onPress")}>
+      Left
+    </Button>
+    <Separator orientation="vertical" />
+    <Button variant="quiet" onPress={action("onPress")}>
+      Right
+    </Button>
+  </>
+)
