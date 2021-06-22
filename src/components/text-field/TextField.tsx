53de12c68dbde542d854dce516039905c02f6231
Siddhant <me@madebysid.com>
2021-06-22 12:18:06 +0200

    Add an error prop to TextField

--- src/components/text-field/TextField.tsx
+++ src/components/text-field/TextField.tsx
@@ -1,12 +1,15 @@
 import React, { useRef, forwardRef } from "react"
 import cn from "classnames"
 import { useTextField } from "@react-aria/textfield"
-import { useFocusWithin } from "@react-aria/interactions"
-import { mergeProps } from "@react-aria/utils"
+import { useFocusWithin, useHover } from "@react-aria/interactions"
 import { Label } from "../label/Label"
 import { FocusRing } from "../focus-ring/FocusRing"
+import { Icon } from "../icon/Icon"
+import { Tooltip } from "../tooltip/Tooltip"
+import { useFocusable } from "@react-aria/focus"
+import { mergeProps } from "@react-aria/utils"
 
-export type TextFieldProps = React.PropsWithChildren<{
+export type TextFieldProps = {
   /** A React ref to attach to the rendered Button */
   ref?: React.ForwardedRef<HTMLInputElement>
   /** An HTML ID attribute that will be attached to the the rendered component. Useful for targeting it from tests */
@@ -15,10 +18,10 @@ export type TextFieldProps = React.PropsWithChildren<{
   autoFocus?: boolean
   /** Initial value to populate the TextField with */
   defaultValue?: string
+  /** An optional error to show below the TextField */
+  error?: string
   /** Hints at the type of data that might be entered into this TextField */
   inputMode?: "text" | "email" | "tel" | "url" | "numeric" | "decimal"
-  /** Additional props to spread over the input component */
-  inputProps?: React.InputHTMLAttributes<HTMLInputElement>
   /** Controls if this TextField is disabled */
   isDisabled?: boolean
   /** Controls if this TextField is readonly */
@@ -37,17 +40,16 @@ export type TextFieldProps = React.PropsWithChildren<{
   type?: "text" | "email" | "tel" | "url" | "numeric" | "decimal"
   /** The value of the TextField */
   value?: string
-}>
+}
 
 export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
   (
     {
       id,
       autoFocus = false,
-      children,
       defaultValue,
+      error,
       inputMode,
-      inputProps = {},
       isDisabled = false,
       isReadOnly = false,
       label,
@@ -62,7 +64,7 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
   ) => {
     const _inputRef = useRef<HTMLInputElement>(null)
     const inputRef = forwardedRef || _inputRef
-    const { labelProps, inputProps: textFieldInputProps } = useTextField(
+    const { labelProps, inputProps } = useTextField(
       {
         id,
         autoFocus,
@@ -76,14 +78,16 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
         placeholder,
         type,
         value,
+        validationState: error ? "invalid" : undefined,
       },
       inputRef as React.RefObject<HTMLInputElement>
     )
 
     const { focusWithinProps } = useFocusWithin({
       isDisabled,
-      // onFocusWithin: () => inputRef.current?.focus(),
     })
+
+    const isInvalid = !!inputProps["aria-invalid"]
 
     return (
       <div className={cn("table-row w-full")}>
@@ -96,19 +100,20 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
                 "flex flex-grow",
                 "rounded-md shadow-sm border border-gray-300 dark:border-gray-700",
                 "text-sm",
+                "overflow-hidden",
                 {
-                  "px-3 py-1.5": !prefix,
                   "pr-1.5": prefix,
                   "bg-gray-100 dark:bg-gray-800": isDisabled,
                   "bg-white dark:bg-gray-900": !isDisabled,
                   "cursor-not-allowed": isDisabled,
+                  "border-2 border-red-500 dark:border-red-500": isInvalid,
                 }
               )}
             >
               {prefix && (
                 <div
                   className={cn(
-                    "mr-3 py-1.5 px-3",
+                    "py-1.5 px-3",
                     "bg-gray-200 dark:bg-gray-700",
                     "text-gray-800 dark:text-gray-400 font-medium",
                     "select-none"
@@ -120,16 +125,14 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
               <input
                 ref={inputRef}
                 lens-role="text-field"
-                {...mergeProps(
-                  inputProps,
-                  textFieldInputProps as React.InputHTMLAttributes<HTMLInputElement>
-                )}
-                className={cn("flex-grow", {
-                  input: true,
+                {...(inputProps as React.InputHTMLAttributes<HTMLInputElement>)}
+                className={cn("flex-grow input", "px-3 py-1.5", {
                   disabled: isDisabled,
                 })}
+                required
               />
-              {children}
+
+              <ErrorIcon text={error} visible={isInvalid} />
             </div>
           </section>
         </FocusRing>
@@ -137,3 +140,44 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
     )
   }
 )
+
+type ErrorIconProps = {
+  /** Controls if the error icon should show */
+  visible: boolean
+  /** The error text */
+  text?: string
+}
+
+function ErrorIcon({
+  text = "This field is invalid",
+  visible,
+}: ErrorIconProps) {
+  const ref = useRef<HTMLDivElement>(null)
+  const { focusableProps } = useFocusable({ excludeFromTabOrder: false }, ref)
+  const { hoverProps, isHovered } = useHover({})
+
+  if (!visible) {
+    return null
+  }
+
+  return (
+    <>
+      <div
+        ref={ref as any}
+        className="p-2"
+        {...mergeProps(focusableProps, hoverProps)}
+      >
+        <Icon
+          name="alert-circle"
+          className="text-red-500 dark:text-red-500"
+          size="sm"
+        />
+      </div>
+      {isHovered && (
+        <Tooltip target={ref} position="right">
+          {text}
+        </Tooltip>
+      )}
+    </>
+  )
+}
